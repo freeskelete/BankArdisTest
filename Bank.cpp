35902479a945ca7ca6d3f3f718d9363cbf322521
freeskelete <solverin20.05@gmail.com>
2024-08-06 11:26:07 +0200

    11

--- Bank.cpp
+++ Bank.cpp
@@ -45,7 +45,20 @@ void Bank::open() {
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
     std::cout << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S.") << std::setw(3) << std::setfill('0') << ms.count() << "] Bank opened" << std::endl;
 
-    processClients();
+    std::thread processingThread(&Bank::processClients, this);
+
+    // Wait until all clients are served
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this] { return allClientsServed(); });
+    }
+
+    // Close the bank
+    close();
+
+    if (processingThread.joinable()) {
+        processingThread.join();
+    }
 }
 
 void Bank::processClients() {
@@ -101,15 +114,9 @@ bool Bank::allClientsServed() const {
 
 
 void Bank::close() {
-    {
-        std::unique_lock<std::mutex> lock(mtx);
-        isBankOpen = false;
-        cv.notify_all();
-    }
-        if (doneClients == totalClients) {
+
             auto now = std::chrono::system_clock::now();
             auto time = std::chrono::system_clock::to_time_t(now);
             auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
             std::cout << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S.") << std::setw(3) << std::setfill('0') << ms.count() << "] Bank closed" << std::endl;
-        }
 }
